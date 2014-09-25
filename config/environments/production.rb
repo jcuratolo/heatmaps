a99d7d0a5deacc9514bd336492bfb732ccd96590
jcuratolo <jcuratolo@me.com>
2014-09-24 20:04:40 -0700

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Heatmaps::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = ['*.js', '*.css']
 
   # Generate digests for assets URLs.
   config.assets.digest = true
